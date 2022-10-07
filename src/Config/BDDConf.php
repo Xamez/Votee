5f33a9710a7292a1ae907d9f96e84a668f80a9ad
cazauxl <loris.cazaux01@etu.umontpellier.fr>
2022-10-07 11:23:07 +0200

    BDD Setup

---
+++ src/Config/BDDConf.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Covoiturage\Config;
+
+class BDDConf {
+
+    static private array $databases = array(
+        'hostname' => 'webinfo',
+        'database' => 'cazauxl',
+        'login' => 'cazauxl',
+        'password' => 'NA2Lz@-m0T0v1DSk'
+    );
+
+    static public function getLogin() : string {
+        return static::$databases['login'];
+    }
+
+    static public function getHostname() : string {
+        return static::$databases['hostname'];
+    }
+
+    static public function getDatabase() : string {
+        return static::$databases['database'];
+    }
+
+    static public function getPassword() : string {
+        return static::$databases['password'];
+    }
+}
