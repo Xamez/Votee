5e6bd610b7dc588a7825b7fcb7869ea89fd2b3c5
Thomas <thomas.nalix123@gmail.com>
2022-10-21 15:28:03 +0200

    Creation de questions et correction des link

--- src/view/question/create.php
+++ src/view/question/create.php
@@ -3,39 +3,39 @@
         <legend>Créer une question :</legend>
         <p>
             <label for="visibilite_id">visibilite</label> :
-            <input type="text" placeholder="Nimes" name="depart" id="visibilite_id" required/>
+            <input type="text" placeholder="visible" name="visibilite" id="visibilite_id" required/>
         </p>
         <p>
             <label for="systeme_vote_id">systemeVoteQuestion</label> :
-            <input type="text" placeholder="Paris" name="arrivee" id="systeme_vote_id" required/>
+            <input type="text" placeholder="Question" name="systemeVoteQuestion" id="systeme_vote_id" required/>
         </p>
         <p>
             <label for="plan_texte_id">planTexteQuestion</label> :
-            <input type="text" placeholder="2022-09-29" name="date" id="plan_texte_id" required/>
+            <input type="text" placeholder="Plan" name="planTexteQuestion" id="plan_texte_id" required/>
         </p>
         <p>
             <label for="date_debut_question_id<_id">dateDebutQuestion</label> :
-            <input type="number" placeholder="5" name="places" id="date_debut_question_id" required/>
+            <input type="date" name="dateDebutQuestion" id="date_debut_question_id" required/>
         </p>
         <p>
             <label for="date_fin_question_id">dateFinQuestion</label> :
-            <input type="number" placeholder="12" name="prix" id="date_fin_question_id" required/>
+            <input type="date" name="dateFinQuestion" id="date_fin_question_id" required/>
         </p>
         <p>
             <label for="date_debut_vote_id">dateDebutVote</label> :
-            <input type="text" placeholder="tnalix" name="login" id="date_debut_vote_id" required/>
+            <input type="date" name="dateDebutVote" id="date_debut_vote_id" required/>
         </p>
         <p>
             <label for="date_fin_vote_id">dateFinVote</label> :
-            <input type="text" placeholder="tnalix" name="login" id="date_fin_vote_id" required/>
+            <input type="date" name="dateFinVote" id="date_fin_vote_id" required/>
         </p>
         <p>
             <label for="categorie_id">idCategorie</label> :
-            <input type="text" placeholder="tnalix" name="login" id="categorie_id" required/>
+            <input type="number" placeholder="1" name="idCategorie" id="categorie_id" required/>
         </p>
         <p>
             <label for="login_id">login</label> :
-            <input type="text" placeholder="tnalix" name="login" id="login_id" required/>
+            <input type="text" placeholder="tjean" name="login" id="login_id" required/>
         </p>
         <p>
             <input type="submit" value="Envoyer" />
