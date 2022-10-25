ee619222ac20574784b399e49215d2b1972db2bc
Xamez <35816109+Xamez@users.noreply.github.com>
2022-10-25 16:59:46 +0200

    Avancement page d'accueil

---
+++ src/view/home.php
@@ -0,0 +1,7 @@
+<div class="flex flex-col md:flex-row items-center">
+    <div class="flex flex-col">
+        <h1 class="text-4xl font-bold drop-shadow-md w-fit"><span class="text-dark">Venez voter en</span><br/><span class="text-main">quelques secondes</span></h1>
+        <h3 class="">Exprime ton opinion en un clic</h3>
+    </div>
+    <img src="../resources/undraw_voting.png" alt="Vote">
+</div>
