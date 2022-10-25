214a805f909842d1aa5060fff11545b92bd6c843
Xamez <35816109+Xamez@users.noreply.github.com>
2022-10-25 12:17:25 +0200

    Légère modification design + désormais responsive

--- src/view/question/detail.php
+++ src/view/question/detail.php
@@ -1,14 +1,27 @@
 <?php
 // affiche le login de l'utilisateur qui a créé la question
-echo '<div class="flex items-center gap-2"><p class="text-main">Représentant : 
-        <div class="flex gap-1 text-main shadow-md rounded-2xl w-fit p-2"><span class="material-symbols-outlined">account_circle</span>' . htmlspecialchars($question->getLogin())
-    . '</div></p></div><div class="flex flex-col gap-5 border-2 p-8 rounded-3xl">';
+echo '<div class="flex items-center gap-2"><p class="text-main font-semibold">Représentant : 
+        <div class="flex gap-1 text-main shadow-md rounded-2xl w-fit p-2"><span class="material-symbols-outlined">account_circle</span>' . htmlspecialchars($question->getLogin()) .
+     '</div></p></div>';
+
+
+// TODO: afficher co-auteurs (EXEMPLE ICI)
+echo '<div class="flex items-center flex-wrap gap-2 pt-0"><p class="text-main font-semibold">Co-auteurs :</p>';
+for($i = 0; $i < 8; $i++)
+    echo '<div class="flex gap-1 text-main shadow-md rounded-2xl p-2"><span class="material-symbols-outlined">account_circle</span>' . htmlspecialchars("A faire") . '</div>';
+echo '</div>';
+
+echo '<div class="flex flex-col gap-5 border-2 p-8 rounded-3xl">';
 $index=0;
 foreach ($sections as $section) {
     $index++;
     $sectionTitreHTML = htmlspecialchars($section->getTitre());
     $sectionDescHTML = htmlspecialchars($section->getTexte());
 
-    echo '<h1 class="text-main text-2xl font-bold">' . $index . ' - ' . $sectionTitreHTML . '</h1><p>'. $sectionDescHTML .'</p>';
+    echo
+    '<h1 class="text-main text-2xl font-bold">'
+        . $index . ' - ' . $sectionTitreHTML .
+    '</h1>
+    <p class="text-justify">'. $sectionDescHTML .'</p>';
 }
 echo '</div>';
