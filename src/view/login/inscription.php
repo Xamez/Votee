8271d10c13008001028f6932bc12e7d025ba7cc9
nalixt <thomas.nalix123@gmail.com>
2022-10-25 17:00:36 +0200

    Connexion et inscription

---
+++ src/view/login/inscription.php
@@ -0,0 +1,18 @@
+<div class="flex flex-col gap-5 items-center pt-6">
+    <form class="flex flex-col gap-10 items-center" method="post" action="">
+        <div class="flex flex-col gap-2">
+            <div>
+                <input type="text" placeholder="cazauxl" name="login"/>
+            </div>
+            <div>
+                <input type="password" name="password"/>
+            </div>
+        </div>
+        <div>
+            <input class="p-2 text-white bg-main font-semibold rounded-lg" type="submit" value="Inscription" />
+        </div>
+    </form>
+    <div class="flex gap-1">
+        <p>Vous n’avez pas de compte ? </p><a class="text-main" href="inscription.php">Connectez-vous.</a>
+    </div>
+</div>
