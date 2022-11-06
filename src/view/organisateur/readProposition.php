a829dbdef836d13060460a091cc8a78ee7dd0f5f
Jasthom <91681828+Jasthom@users.noreply.github.com>
2022-11-06 13:11:19 +0100

    Implémentation des pages Delete

--- src/view/organisateur/readProposition.php
+++ src/view/organisateur/readProposition.php
@@ -10,7 +10,7 @@ foreach ($sections as $index=>$section) {
 }
 echo '</div><div class="flex gap-2 justify-end">
         <a class="w-36 flex p-2 justify-center text-white bg-dark font-semibold rounded-lg" 
-            href="./frontController.php?action=deletePropsition&idProposition=' . rawurlencode($idProposition) . '">Supprimer</a>
+            href="./frontController.php?action=deleteProposition&idProposition=' . rawurlencode($idProposition) . '">Supprimer</a>
         <a class="w-36 flex p-2 justify-center text-white bg-main font-semibold rounded-lg" 
             href="./frontController.php?action=update&idQuestion=' . rawurlencode($question->getIdQuestion()). '&idProposition='. rawurlencode($idProposition) . '">Editer</a>
 </div>';
