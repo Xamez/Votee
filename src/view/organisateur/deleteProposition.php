e5cecd4342f3b5183b4762112d05968edd85a224
Jasthom <thomas.nalix123@gmail.com>
2022-11-09 09:28:57 +0100

    Gestion des injections

--- src/view/organisateur/deleteProposition.php
+++ src/view/organisateur/deleteProposition.php
@@ -1,7 +1,9 @@
 <div class="flex flex-col justify-center items-center gap-7">
     <h1>Cette action supprimera définitivement la proposition</h1>
     <div class="flex items-center gap-7">
-        <a class="p-2 w-48 text-center text-white bg-main font-semibold rounded-lg" href="./frontController.php?action=deletedProposition&idProposition=<?= $idProposition ?>">Oui</a>
-        <a class="p-2 w-48 text-center text-white bg-main font-semibold rounded-lg" href="./frontController.php?action=readAllQuestion">Non</a>
+        <a class="p-2 w-48 text-center text-white bg-main font-semibold rounded-lg"
+           href="./frontController.php?action=deletedProposition&idProposition=<?= rawurldecode($idProposition) ?>">Oui</a>
+        <a class="p-2 w-48 text-center text-white bg-main font-semibold rounded-lg"
+           href="./frontController.php?action=readAllQuestion">Non</a>
     </div>
 </div>
