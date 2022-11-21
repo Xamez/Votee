9d06460b0fd9c605e1b653a2626920d9f3405ab0
Xamez <35816109+Xamez@users.noreply.github.com>
2022-11-21 15:30:10 +0100

    :sparkles: Ajout période sur la liste des questions

--- src/view/organisateur/listQuestion.php
+++ src/view/organisateur/listQuestion.php
@@ -5,8 +5,18 @@ if($questions) {
         $questionTitreHTML = htmlspecialchars($question->getTitre());
         $questionIdURL = rawurlencode($question->getIdQuestion());
 
-        echo '<a href="./frontController.php?action=readQuestion&idQuestion=' . $questionIdURL . '">
-        <div class="flex justify-between bg-light p-2 rounded">' . $questionTitreHTML . '<span class="material-symbols-outlined">arrow_forward_ios</span></div></a>';
+        echo '
+        <a href="./frontController.php?action=readQuestion&idQuestion=' . $questionIdURL . '">
+        <div class="flex justify-between items-center bg-light p-2 rounded">
+            ' . $questionTitreHTML . '
+            <div class="flex items-center gap-2">
+                <div class="bg-white flex text-main shadow-md rounded-2xl w-fit p-1.5">
+                            ' . $question->getPeriodeActuelle() . '
+                </div>
+                <span class="material-symbols-outlined">arrow_forward_ios</span>
+            </div>
+        </div>
+        </a>';
     }
 }
 echo '</div><a class="w-36 flex p-2 justify-center text-white bg-main font-semibold rounded-lg" href="./frontController.php?action=section">Créer un vote</a>';
