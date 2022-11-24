f9aabc71659148142642e07250f3184a415052fa
Xamez <35816109+Xamez@users.noreply.github.com>
2022-11-24 22:41:36 +0100

    Début mise en place commentaire (temporaire)

--- web/js/nav-burger.js
+++ web/js/nav-burger.js
@@ -1,8 +1,52 @@
+let popup;
+
 window.onload = () => {
 
     const navBurger = document.getElementById('nav-burger');
     const openIcon = document.getElementById('open-icon');
     const closeIcon = document.getElementById('close-icon');
+
+    popup = document.getElementById('popup');
+    const createCommentary = document.getElementById('create-commentary');
+
+    let nbParagraph;
+    let indexCharacter;
+
+    let moved = false;
+
+    createCommentary.addEventListener('click', () => {
+        console.log("C'est créer");
+    });
+
+    const performHidePopup = (e) => {
+        if (!moved && !popup.contains(e.target))
+            popup.style.display = "none";
+    }
+
+    document.addEventListener('mousedown', () => {   
+        moved = false;
+    });
+
+    document.addEventListener('mouseup', e => performHidePopup(e));
+
+    document.addEventListener('mousemove', (e) => {
+        performHidePopup(e);
+        moved = true;
+    });
+
+    document.addEventListener('selectionchange', (e) => {
+        const selection = document.getSelection();
+        if (selection.type === 'Range' && popup.style.display === "none") {
+            performHidePopup(e);
+            const selectedParagraph = selection.anchorNode.parentElement;
+            const selectedText = selection.toString();
+            nbParagraph = selectedParagraph.id;
+            indexCharacter = selectedParagraph.innerHTML.indexOf(selectedText);
+            popup.style.display = "block";
+            popup.style.top = `${selectedParagraph.offsetTop + selectedParagraph.offsetHeight}px`;
+            popup.style.left = `${window.innerWidth / 2 - popup.offsetWidth / 2}px`;
+        }
+    });
 
     const toogleNav = () => {
         navBurger.classList.toggle('hidden');
@@ -14,3 +58,7 @@ window.onload = () => {
     closeIcon.addEventListener('click', toogleNav);
 
 };
+
+window.onbeforeunload = () => {
+    popup.style.display = "none";
+}
