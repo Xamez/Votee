cc7dc69bffd671ea0780757d9fd90fc08e23e9b5
Xamez <35816109+Xamez@users.noreply.github.com>
2022-11-27 18:46:05 +0100

    :art: Suppression de code inutile

--- web/js/accordion.js
+++ web/js/accordion.js
@@ -1,22 +1,9 @@
 window.onload = () => {
 
-    const navBurger = document.getElementById('nav-burger');
-    const openIcon = document.getElementById('open-icon');
-    const closeIcon = document.getElementById('close-icon');
-
     const accordions = document.getElementsByClassName("accordion");
 
-    const toogleNav = () => {
-        navBurger.classList.toggle('hidden');
-        openIcon.classList.toggle('hidden');
-        closeIcon.classList.toggle('hidden');
-    };
-
-    openIcon.addEventListener('click', toogleNav);
-    closeIcon.addEventListener('click', toogleNav);
-
     for (let i = 0; i < accordions.length; i++) {
-        accordions[i].addEventListener("click", function() {
+        accordions[i].addEventListener("click", () => {
             this.classList.toggle("active");
             let panel = this.nextElementSibling;
             panel.style.display = (panel.style.display === "block") ? "none" : "block";
