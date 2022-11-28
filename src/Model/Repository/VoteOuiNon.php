0e82d3502a43296f9f374c64e21dd03cbaa0a394
Alex <alexafo31@gmail.com>
2022-11-28 22:00:27 +0100

    Créations des votes oui non

---
+++ src/Model/Repository/VoteOuiNon.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Votee\Model\Repository;
+
+class VoteOuiNon
+{
+
+}
--- src/view/organisateur/readProposition.php
+++ src/view/organisateur/readProposition.php
@@ -48,4 +48,16 @@ if ($question->getPeriodeActuelle() == 'Période de vote'){
          </div>
         ';
     }
+    elseif ($question->getTypeVote() == 'VoteOuiNon') {
+        echo '
+        <div class="flex">
+            <div class="p-3 rounded-l-xl w-28 text-white font-semibold" style="background-color: #b8b8f8">
+                <a href="./frontController.php?action=createVote&idQuestion=' . rawurlencode($question->getIdQuestion()) . '&idProposition=' . rawurlencode($idProposition) . '&value=0">Non</a>
+            </div>
+             <div class="p-3 rounded-xl w-28 -ml-3.5 text-white font-semibold text-center" style="background-color: #aea4ff">
+               <a href="./frontController.php?action=createVote&idQuestion=' . rawurlencode($question->getIdQuestion()) . '&idProposition=' . rawurlencode($idProposition) . '&value=1">Oui</a>
+             </div>
+         </div>
+        ';
+    }
 }
