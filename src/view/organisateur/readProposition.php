b7fdcad625b3c7138f30418682812ce7e61e0352
Jasthom <91681828+Jasthom@users.noreply.github.com>
2022-11-29 19:34:41 +0100

    Correction des problème de merge

--- src/view/organisateur/readProposition.php
+++ src/view/organisateur/readProposition.php
@@ -3,26 +3,27 @@ require "propositionHeader.php";
 echo '</p><div class="flex flex-col gap-5 border-2 p-8 rounded-3xl">';
 foreach ($sections as $index=>$section) {
     $sectionTitreHTML = htmlspecialchars($section->getTitreSection());
-    $sectionDescHTML = htmlspecialchars($textes[$index]->getTexte());
+    $sectionDescHTML = $textes[$index]->getTexte();
 
     echo '<h1 class="text-main text-2xl font-bold">'. $index + 1 . ' - ' . $sectionTitreHTML . '</h1>
-              <p class="break-all text-justify">' . $sectionDescHTML . '</p>';
+              <div class="proposition-markdown break-all text-justify">' . $sectionDescHTML . '</div>';
 }
-echo '</div>
-        <div class="flex gap-2 justify-between">
-            <a href="./frontController.php?action=updateProposition&idQuestion=' . rawurlencode($question->getIdQuestion()). '&idProposition='. rawurlencode($idProposition) . '">
+echo '</div>';
+if ($question->getPeriodeActuelle() == 'Période d\'écriture') {
+    echo '<div class="flex gap-2 justify-between">
+            <a href="./frontController.php?action=updateProposition&idQuestion=' . rawurlencode($question->getIdQuestion()) . '&idProposition=' . rawurlencode($idProposition) . '">
                 <div class="flex gap-2">
                     <span class="material-symbols-outlined">edit</span>
                     <p>Editer</p>
                 </div
             </a>
-            <a href="./frontController.php?action=selectFusion&idQuestion=' . rawurlencode($question->getIdQuestion()). '&idProposition='. rawurlencode($idProposition) . '">
+            <a href="./frontController.php?action=selectFusion&idQuestion=' . rawurlencode($question->getIdQuestion()) . '&idProposition=' . rawurlencode($idProposition) . '">
                 <div class="flex gap-2">
                     <span class="material-symbols-outlined">upload</span>
                     <p>Fusionner</p>
                 </div
             </a>
-            <a href="./frontController.php?action=deleteProposition&idProposition=' . rawurlencode($idProposition) . '">
+            <a href="./frontController.php?action=deleteProposition&idQuestion=' . rawurlencode($question->getIdQuestion()) . '&idProposition=' . rawurlencode($idProposition) . '">
                 <div class="flex gap-2">
                     <p>Supprimer</p>
                     <span class="material-symbols-outlined">delete</span>
@@ -49,7 +50,7 @@ if ($question->getPeriodeActuelle() == 'Période de vote'){
          </div>
         ';
     }
-    elseif ($question->getTypeVote() == 'VoteOuiNon') {
+    else if ($question->getTypeVote() == 'VoteOuiNon') {
         echo '
         <div class="flex">
             <div class="p-3 rounded-l-xl w-28 text-white font-semibold" style="background-color: #b8b8f8">
