251864c3bfdccfaf74ca46bf9c91cf1a4a28dcc3
Xamez <35816109+Xamez@users.noreply.github.com>
2022-12-15 12:11:43 +0100

    :sparkles: Début mise en place des résultats

--- src/view/proposition/vote/resultat/jugementMajoritaire.php
+++ src/view/proposition/vote/resultat/jugementMajoritaire.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "C LE DESIGN DE RESULTAT DE JUGEMENT MAJORITAIRE";
+$colors = ['', '', '', ''];
+
+echo '<div class="">';
+
+foreach ($propositions as $proposition) {
+    $idProposition = $proposition->getIdProposition();
+    echo '<div class="flex flex-row">';
+    for ($i = 0; $i < sizeof($values[$idProposition]); $i++) {
+        echo '
+        <div class="" style="width: ' . $values[$idProposition][$i] . '%">
+            
+        </div>
+    ';
+    }
+    echo '</div>';
+}
+
+echo '</div>';
