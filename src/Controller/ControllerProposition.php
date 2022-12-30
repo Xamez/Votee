cca89dd436d05877882d3d6c976ef09ac859603a
Xamez <35816109+Xamez@users.noreply.github.com>
2022-12-30 20:40:53 +0100

    :art: Déplacement du visionnage de spécialiste de readProposition à readQuestion

--- src/Controller/ControllerProposition.php
+++ src/Controller/ControllerProposition.php
@@ -317,7 +317,6 @@ class ControllerProposition extends AbstractController {
             self::redirection("?controller=question&action=all");
         }
         $question = (new QuestionRepository())->select($_GET['idQuestion']);
-        $specialiste = (new UtilisateurRepository())->select($question->getLoginSpecialiste());
         $proposition = (new PropositionRepository())->select($_GET['idProposition']);
         $textes = (new TexteRepository())->selectAllByKey($_GET['idProposition']);
         $filsRaw = (new PropositionRepository())->getFilsFusion($_GET['idProposition']);
@@ -344,7 +343,6 @@ class ControllerProposition extends AbstractController {
                     "sections" => $sections,
                     "coAuteurs" => $coAuteurs,
                     "textes" => $textes,
-                    "specialiste" => $specialiste,
                     "responsable" => $responsable,
                     "pagetitle" => "Question",
                     "cheminVueBody" => "proposition/readProposition.php",
