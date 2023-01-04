d0c38dff701602c5a04bd1b40393d95009992c0b
cazauxl <loris.cazaux01@etu.umontpellier.fr>
2023-01-04 17:48:02 +0100

    commencement des utilisateurs quand on clique

--- src/view/utilisateur/readUtilisateur.php
+++ src/view/utilisateur/readUtilisateur.php
@@ -1,9 +1,20 @@
 <h1 class="title text-dark text-2xl font-semibold">Informations</h1>
-<?php echo $utilisateurC->getPrenom() . ' ' . $utilisateurC->getNom();
-?>
+
+<!-- créer une carte utilisateur avec logo sur la gauche et toutes les informations sur la droite (nom, prénom, description) dans des champs avec des flex tout ça tenant sur la hauteur de l'image de profil-->
+<div class="flex justify-center gap-2">
+    <div>
+        <span style="font-size: 250px" class="material-symbols-outlined select-none">face</span>
+    </div>
+    <div class="flex flex-col gap-3 text-xl justify-start">
+        <span><?=htmlspecialchars($utilisateurC->getPrenom()). ' ' . htmlspecialchars($utilisateurC->getNom())?></span>
+        <span>description olala</span>
+    </div>
+</div>
+
 
 <h1 class="title text-dark text-2xl font-semibold">Groupes</h1>
 <div class="flex flex-wrap gap-2 justify-center">
+
 <?php
 foreach ($groupes as $groupe) {
     echo '<a href="./frontController.php?controller=groupe&action=readGroupe&idGroupe=' . rawurlencode($groupe->getIdGroupe()) . '">
@@ -13,4 +24,5 @@ foreach ($groupes as $groupe) {
           </a>';
 }
 ?>
+
 </div>
