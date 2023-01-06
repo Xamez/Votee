71272632d795d82057d725a895ba7418fbb32d60
Jasthom <thomas.nalix123@gmail.com>
2023-01-06 17:25:54 +0100

    Responsive

--- src/view/button.php
+++ src/view/button.php
@@ -1,8 +1,8 @@
 <?php
-echo '<a class="bg-white shadow-around p-2 rounded-2xl" href="./frontController.php?controller=' . $controller . '&action=' . $action;
+echo '<a class="flex bg-white shadow-around p-2 rounded-2xl" href="./frontController.php?controller=' . $controller . '&action=' . $action;
 if (isset($params)) echo '&' . $params;
 echo '">
-    <div class="flex gap-2 justify-center">
+    <div class="flex gap-2 justify-center items-center">
         <span class="material-symbols-outlined">' . $logo . '</span>
         <p>' . $title . '</p>
     </div>
---
+++ src/view/question/proposition.php
@@ -0,0 +1,15 @@
+<a href="./frontController.php?controller=proposition&action=readProposition&idQuestion=<?=$idQuestion . '&idProposition=' . $idProposition ?>">
+    <div class="flex bg-light justify-between p-3 md:p-2 items-center rounded">
+        <div class="flex justify-items-start md:items-center gap-3 md:gap-2 md:flex-row flex-col">
+            <p class="font-bold text-dark hidden md:block">Proposition de : </p>
+            <div class="bg-white flex gap-1 text-main shadow-md rounded-2xl w-fit p-2">
+                <span class="material-symbols-outlined">account_circle</span><?=htmlspecialchars($responsable->getPrenom()) . ' ' . htmlspecialchars($responsable->getNom()) ?>
+            </div>
+            <span><?= htmlspecialchars($proposition->getTitreProposition()) ?></span>
+        </div>
+        <div class="flex gap-2 md:flex-row flex-col">
+           <?= (!$proposition->isVisible() ? '<span class="material-symbols-outlined">inventory_2</span>' : '') ?>
+            <span class="material-symbols-outlined">arrow_forward_ios</span>
+        </div>
+    </div>
+</a>
