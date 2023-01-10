6f8e192e14e3009b3c2735f2509f24202cf2319b
Xamez <35816109+Xamez@users.noreply.github.com>
2023-01-10 16:59:41 +0100

    :bug: Fix bug checkbox sur autres navigateur

--- src/view/question/addVotant.php
+++ src/view/question/addVotant.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="assets/js/checkBox.js"></script>
 <form class="flex flex-col gap-10" method="post" action="frontController.php?controller=question&action=addedVotant">
     <h1 class="title text-dark text-2xl font-semibold">Groupes</h1>
     <div class="flex flex-wrap gap-2 justify-center">
@@ -5,14 +6,14 @@
 
         foreach ($groupes as $key=> $groupe) {
             echo '<div class="border-2 border-transparent util-box text-main bg-white items-center shadow-md rounded-2xl w-fit p-2">
-                    <input class="utilCheck" type="checkbox" name="groupesExist[]" id="groupeExist' . $key . '" value="' . $groupe->getIdGroupe() . '" checked/>
+                    <input type="checkbox" name="groupesExist[]" id="groupeExist' . $key . '" value="' . $groupe->getIdGroupe() . '" checked/>
                     <label class="cursor-pointer flex gap-1 items-center" for="groupeExist' . $key . '"><span class="material-symbols-outlined">group</span>' . $groupe->getNomGroupe() . '</label>
                   </div>';
         }
 
         foreach ($newGroupes as $key=> $groupe) {
             echo '<div class="cursor-pointer border-2 border-transparent util-box items-center text-main bg-white shadow-md rounded-2xl w-fit p-2">
-                    <input class="utilCheck" type="checkbox" name="groupes[]" id="groupe' . $key . '" value="' . $groupe->getIdGroupe() . '"/>
+                    <input type="checkbox" name="groupes[]" id="groupe' . $key . '" value="' . $groupe->getIdGroupe() . '"/>
                     <label class="cursor-pointer flex gap-1 items-center" for="groupe' . $key . '"><span class="material-symbols-outlined">group</span>' . $groupe->getNomGroupe() . '</label>
                   </div>';
         }
@@ -29,14 +30,14 @@
 
         foreach ($votants as $key=>$votant) {
                 echo '<div class="border-2 border-transparent util-box text-main items-center bg-white shadow-md rounded-2xl w-fit p-2">
-                        <input class="votantCheck" type="checkbox" name="votants[]" id="votant' . $key . '" value="' . $votant->getLogin() . '" checked/>
+                        <input type="checkbox" name="votants[]" id="votant' . $key . '" value="' . $votant->getLogin() . '" checked/>
                         <label class="cursor-pointer flex gap-1 items-center" for="votant' . $key . '"><span class="material-symbols-outlined">account_circle</span>' . $votant->getPrenom() . ' ' . $votant->getNom() . '</label>
                       </div>';
         }
 
         foreach ($newUtilisateurs as $key=>$utilisateur) {
                 echo '<div class="border-2 border-transparent util-box text-main items-center bg-white shadow-md rounded-2xl w-fit p-2">
-                        <input class="utilCheck" type="checkbox" name="utilisateurs[]" id="util' . $key . '" value="' . $utilisateur->getLogin() . '"/>
+                        <input type="checkbox" name="utilisateurs[]" id="util' . $key . '" value="' . $utilisateur->getLogin() . '"/>
                         <label class="cursor-pointer flex gap-1 items-center" for="util' . $key . '"><span class="material-symbols-outlined">account_circle</span>' . $utilisateur->getPrenom() . ' ' . $utilisateur->getNom() . '</label>
                       </div>';
         }
