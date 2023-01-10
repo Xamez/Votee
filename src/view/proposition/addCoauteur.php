5354f4339e5435c152f9b3694f48dac078bcefbc
Tourniayre Maxence <maxence.tourniayre@etu.umontpellier.fr>
2023-01-10 16:59:41 +0100

    :bug: Fix bug checkbox sur autres navigateur

--- src/view/proposition/addCoauteur.php
+++ src/view/proposition/addCoauteur.php
@@ -1,17 +1,18 @@
+<script type="text/javascript" src="assets/js/checkBox.js"></script>
 <h1 class="title text-dark text-2xl font-semibold">Utilisateurs</h1>
 <form class="flex flex-col gap-10" method="post" action="frontController.php?controller=proposition&action=addedCoauteur">
     <div class="flex flex-wrap gap-2 justify-center">
         <?php
         foreach ($coAuteurs as $key=>$coAuteur) {
             echo '<div class="border-2 border-transparent items-center util-box text-main bg-white shadow-md rounded-2xl w-fit p-2">
-                <input class="utilCheck" type="checkbox" name="coAuteurs[]" id="coAuteur' . $key . '" value="' . $coAuteur->getLogin() . '" checked/>
+                <input type="checkbox" name="coAuteurs[]" id="coAuteur' . $key . '" value="' . $coAuteur->getLogin() . '" checked/>
                 <label class="cursor-pointer flex gap-1 items-center" for="coAuteur' . $key . '"><span class="material-symbols-outlined">account_circle</span>' . $coAuteur->getPrenom() . ' ' . $coAuteur->getNom() . '</label>
               </div>';
         }
 
         foreach ($utilisateurs as $key=>$utilisateur) {
             echo '<div class="border-2 border-transparent items-center util-box text-main bg-white shadow-md rounded-2xl w-fit p-2">
-                <input class="utilCheck" type="checkbox" name="utilisateurs[]" id="util' . $key . '" value="' . $utilisateur->getLogin() . '"/>
+                <input type="checkbox" name="utilisateurs[]" id="util' . $key . '" value="' . $utilisateur->getLogin() . '"/>
                 <label class="cursor-pointer flex gap-1 items-center" for="util' . $key . '"><span class="material-symbols-outlined">account_circle</span>' . $utilisateur->getPrenom() . ' ' . $utilisateur->getNom() . '</label>
               </div>';
         }
--- src/view/question/addResp.php
+++ src/view/question/addResp.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="assets/js/checkBox.js"></script>
 <form class="flex flex-col gap-10" method="post" action="frontController.php?controller=question&action=addedResp">
     <h1 class="title text-dark text-2xl font-semibold">Responsables</h1>
     <div class="flex flex-wrap gap-2 justify-center">
@@ -9,15 +10,15 @@
         }
 
         foreach ($responsablesPossibles as $key=>$responsable) {
-            echo '<div class="border-2 border-transparent util-box text-main items-center bg-white shadow-md rounded-2xl w-fit p-2">
-                    <input class="respCheck" type="checkbox" name="resps[]" id="resp' . $key . '" value="' . $responsable->getLogin() . '" checked/>
+            echo '<div class="border-2 border-transparent text-main items-center bg-white shadow-md rounded-2xl w-fit p-2">
+                    <input type="checkbox" name="resps[]" id="resp' . $key . '" value="' . $responsable->getLogin() . '" checked/>
                     <label class="cursor-pointer flex gap-1 items-center" for="resp' . $key . '"><span class="material-symbols-outlined">account_circle</span>' . $responsable->getPrenom() . ' ' . $responsable->getNom() . '</label>
                   </div>';
         }
 
         foreach ($utilisateurs as $key=>$utilisateur) {
             echo '<div class="border-2 border-transparent util-box text-main items-center bg-white shadow-md rounded-2xl w-fit p-2">
-                    <input class="utilCheck" type="checkbox" name="utilisateurs[]" id="util' . $key . '" value="' . $utilisateur->getLogin() . '"/>
+                    <input type="checkbox" name="utilisateurs[]" id="util' . $key . '" value="' . $utilisateur->getLogin() . '"/>
                     <label class="cursor-pointer flex gap-1 items-center" for="util' . $key . '"><span class="material-symbols-outlined">account_circle</span>' . $utilisateur->getPrenom() . ' ' . $utilisateur->getNom() . '</label>
                   </div>';
         }
