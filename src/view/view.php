d7aa68bb92d38b9fa70f00b16a658006e28409f1
Xamez <34zemax34@gmail.com>
2023-01-10 21:47:41 +0100

    :sparkles: Ajout du tag permettant le responsive

--- src/view/view.php
+++ src/view/view.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $pagetitle; ?></title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="assets/css/tailwindcss.min.css">
